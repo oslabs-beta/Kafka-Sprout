f8cdd4f56ee9b58685194ea6012e5f55f025a8a8
Midori Yang <myang5@wellesley.edu>
2020-07-15 19:46:34 -0400

    remove multithreading

--- demo/src/main/java/com/example/lol/Main.java
+++ demo/src/main/java/com/example/lol/Main.java
@@ -1,16 +1,11 @@
 package com.example.lol;
 
-import java.util.concurrent.locks.ReentrantLock;
-
 public class Main {
 
   public static void main(String[] args) {
-    ReentrantLock serverLock = new ReentrantLock();
     String configPath = "C:\\kafka_2.12-2.5.0\\config\\";
     String OS = System.getProperty("os.name").toLowerCase();
-    RunZoo zooThread = new RunZoo(configPath, OS, serverLock);
-    //RunKafka kafkaThread = new RunKafka(configPath, OS, serverLock);
-    zooThread.start();
-    //kafkaThread.start();
+    RunZoo zooThread = new RunZoo(configPath, OS);
+    zooThread.run();
   }
 }
