ccfa6aa93eb29f51cf763cce2ae05683535e399c
Midori Yang <myang5@wellesley.edu>
2020-07-15 15:52:36 -0400

    start kafka thread in zoo thread

--- demo/src/main/java/com/example/lol/Main.java
+++ demo/src/main/java/com/example/lol/Main.java
@@ -7,9 +7,10 @@ public class Main {
   public static void main(String[] args) {
     ReentrantLock serverLock = new ReentrantLock();
     String configPath = "C:\\kafka_2.12-2.5.0\\config\\";
-    RunZoo zooThread = new RunZoo(configPath, serverLock);
-    RunKafka kafkaThread = new RunKafka(configPath, serverLock);
+    String OS = System.getProperty("os.name").toLowerCase();
+    RunZoo zooThread = new RunZoo(configPath, OS, serverLock);
+    //RunKafka kafkaThread = new RunKafka(configPath, OS, serverLock);
     zooThread.start();
-    kafkaThread.start();
+    //kafkaThread.start();
   }
 }
--- demo/src/main/java/com/example/lol/RunKafka.java
+++ demo/src/main/java/com/example/lol/RunKafka.java
@@ -1,7 +1,6 @@
 package com.example.lol;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.concurrent.locks.ReentrantLock;
@@ -9,17 +8,22 @@ import java.util.concurrent.locks.ReentrantLock;
 public class RunKafka extends Thread {
 
   private String path;
+  private String OS;
   private ReentrantLock serverLock;
 
-  public RunKafka(String path, ReentrantLock serverLock) {
+  public RunKafka(String path, String OS, ReentrantLock serverLock) {
     this.path = path;
+    this.OS = OS;
     this.serverLock = serverLock;
   }
 
   @Override
   public void run() {
+    String[] command = new String[2];
+    command[0] = OS.contains("windows") ? "kafka-server-start.bat" : "kafka-server-start";
+    command[1] = path + "server.properties";
 
-    String[] command = { "kafka-server-start.bat", path + "server.properties" };
+    //String[] command = { "kafka-server-start.bat", path + "server.properties" };
     ProcessBuilder processBuilder = new ProcessBuilder(command);
 
     try {
