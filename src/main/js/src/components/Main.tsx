c95522c6febfb6d7f6a5881360a4156abc1ba4ff
Nicole Ip <nicoleapril29@gmail.com>
2020-07-21 19:41:44 -0500

    Added state App to check state of Zookeeper and Kafka

--- src/main/js/src/components/Main.tsx
+++ src/main/js/src/components/Main.tsx
@@ -2,16 +2,25 @@ import React from "react";
 import { Topics } from "./Topics";
 import { StartCluster } from "./StartCluster";
 import { ModalBackground } from "../UIComponents/StyledModal";
+import { RootDiv } from "../UIComponents/UIComponents";
 
-const Main = () => {
-  return (
-    <div>
-      <ModalBackground>
+const Main = (props) => {
+  if (props.status === "false") {
+    return (
+      <div>
+        <ModalBackground>
+          <Topics />
+        </ModalBackground>
+        <StartCluster />
+      </div>
+    );
+  } else {
+    return (
+      <RootDiv>
         <Topics />
-      </ModalBackground>
-      <StartCluster />
-    </div>
-  );
+      </RootDiv>
+    );
+  }
 };
 
 export default Main;
