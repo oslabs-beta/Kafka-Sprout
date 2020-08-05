ec90cbb4d19f226ad0b73d0ef2f80d05a7520408
Nicole Ip <nicoleapril29@gmail.com>
2020-08-05 09:53:16 -0500

    Added graph to monitor Response Rate

--- src/main/js/src/components/MetricsDisplay.tsx
+++ src/main/js/src/components/MetricsDisplay.tsx
@@ -9,13 +9,13 @@ const MetricsDisplay = () => {
   const [waitTime, setWaitTime] = useState(0);
   const [requestTotal, setRequestTotal] = useState(0);
   const [networkRate, setNetworkRate] = useState([]);
+  const [responseRate, setResponseRate] = useState([]);
 
-  const data = {
+  const networkData = {
     labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     datasets: [
       {
-        label:
-          "The number of network operations (reads or writes) on all connections per second",
+        label: "Network I/O Rate (/s)",
         fill: false,
         lineTension: 0.1,
         backgroundColor: "rgba(75,192,192,0.4)",
@@ -38,14 +38,40 @@ const MetricsDisplay = () => {
     ],
   };
 
-  const options = {
+  const responseData = {
+    labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    datasets: [
+      {
+        label: "Response Rate (/s)",
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: responseRate,
+      },
+    ],
+  };
+
+  const networkOptions = {
     scales: {
       yAxes: [
         {
           display: true,
           ticks: {
-            suggestedMin: -1, // minimum will be 0, unless there is a lower value.
-            // OR //
+            suggestedMin: -1, // minimum will be 0, unless there is a lower value
             suggestedMax: 2,
           },
         },
@@ -53,44 +79,18 @@ const MetricsDisplay = () => {
     },
   };
 
-  // const data = {
-  //   datasets: [
-  //     {
-  //       label: "Dataset 1",
-  //       borderColor: "rgb(255, 99, 132)",
-  //       backgroundColor: "rgba(255, 99, 132, 0.5)",
-  //       lineTension: 0,
-  //       borderDash: [8, 4],
-  //       data: [],
-  //     },
-  //   ],
-  // };
-
-  // const options = {
-  //   scales: {
-  //     xAxes: [
-  //       {
-  //         type: "realtime",
-  //         realtime: {
-  //           onRefresh: function () {
-  //             data.datasets[0].data.push({
-  //               x: Date.now(),
-  //               y: Math.random() * 100,
-  //             });
-  //           },
-  //           delay: 2000,
-  //         },
-  //       },
-  //     ],
-  //   },
-  // };
-
-  const handleClick = () => {
-    console.log("im button");
-    clientRef.current.sendMessage(
-      "/app/metrics",
-      JSON.stringify({ message: "FROM SOCKET" })
-    );
+  const responseOptions = {
+    scales: {
+      yAxes: [
+        {
+          display: true,
+          ticks: {
+            suggestedMin: -1,
+            suggestedMax: 1,
+          },
+        },
+      ],
+    },
   };
 
   const client = (
@@ -101,14 +101,20 @@ const MetricsDisplay = () => {
         console.log(msg);
         setWaitTime(Number(msg["io-wait-time-ns-avg"]));
         setRequestTotal(Number(msg["request-total"]));
-        let rate = networkRate.concat(Number(msg["network-io-rate"]));
-        // let rate = Number(msg["network-io-rate"]);
-        if (rate.length > 11) {
-          rate = rate.slice(1);
+        let network = networkRate.concat(Number(msg["network-io-rate"]));
+        if (network.length > 11) {
+          network = network.slice(1);
         }
 
-        setNetworkRate(rate);
+        setNetworkRate(network);
         console.log(networkRate);
+
+        let response = responseRate.concat(Number(msg["response-rate"]));
+        if (response.length > 11) {
+          response = response.slice(1);
+        }
+
+        setResponseRate(response);
       }}
       ref={(client) => {
         clientRef.current = client;
@@ -116,21 +122,21 @@ const MetricsDisplay = () => {
     />
   );
 
-  //"The number of responses received per second" DO NOT DELETE
   return (
     <>
       <div>
-        The average length of time the I/O thread spent waiting for a socket
-        ready for reads or writes in nanoseconds
+        Average I/O Time (/ns)
         {waitTime}
       </div>
       <div>
-        The total number of requests sent
+        Total Requests
         {requestTotal}
       </div>
       {client}
-      <Button onClick={handleClick}>Send Socket</Button>
-      <Line data={data} width={500} options={options} />
+      <div>
+        <Line data={networkData} options={networkOptions} />
+        <Line data={responseData} options={responseOptions} />
+      </div>
     </>
   );
 };
