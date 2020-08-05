bd23c99a95a712c9bddbb7fb53be4f73bdd790ec
nydkim <nydkim@gmail.com>
2020-08-05 18:45:33 -0400

    HomeController removed from SmokeTest

--- src/test/java/com/kafkasprout/backend/SmokeTests.java
+++ src/test/java/com/kafkasprout/backend/SmokeTests.java
@@ -1,14 +1,12 @@
 package com.kafkasprout.backend;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import com.kafkasprout.backend.controllers.ClusterController;
-import com.kafkasprout.backend.controllers.HomeController;
 import com.kafkasprout.backend.controllers.TopicsController;
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class SmokeTests {
@@ -16,15 +14,11 @@ public class SmokeTests {
     private ClusterController clusterController;
 
     @Autowired
-    private HomeController homeController;
-
-    @Autowired
     private TopicsController topicsController;
 
     @Test
     public void contextLoads() throws Exception{
         assertThat(clusterController).isNotNull();
-        assertThat(homeController).isNotNull();
         assertThat(topicsController).isNotNull();
     }
 }
