1864c517200cdce42eec5eaa18a17bb3839c9272
nydkim <nydkim@gmail.com>
2020-08-06 11:13:12 -0400

    minor bug fixes

--- src/main/js/src/components/BrokerConfig.tsx
+++ src/main/js/src/components/BrokerConfig.tsx
@@ -53,7 +53,6 @@ export const BrokerConfig: React.FC<Props> = (props: Props) => {
 
   const handleSubmit = () => {
     const validateConfig = { ...config };
-    // C:\kafka_2.12-2.5.0\config --> C:\\kafka_2.12-2.5.0\\config
     validateConfig.directory = validateConfig.directory.replace(/\\/g, '\\\\');
     validateConfig.properties = validateConfig.properties.replace(/\\/g, '\\\\');
     setLoading(true);
@@ -115,7 +114,7 @@ export const BrokerConfig: React.FC<Props> = (props: Props) => {
         value={config.properties}
       />
       {loading ? <Loader type="TailSpin" color={constants.LIGHTER_GREEN} height={30} width={30} /> : <Button onClick={handleSubmit}>Start Broker</Button>}
-      {error.length > 0 && <div>"please try again \n" {error}</div>}
+      {error.length > 0 && <div>{"please try again \n"} {error}</div>}
     </PopupContainer>
   );
 };
