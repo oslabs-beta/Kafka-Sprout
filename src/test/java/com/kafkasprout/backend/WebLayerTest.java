05a982411a621a1b09d8c6f1dd0216483f7922f7
Guest <Guest@Mac.local>
2020-08-05 20:25:07 -0400

    added more tests

---
+++ src/test/java/com/kafkasprout/backend/WebLayerTest.java
@@ -0,0 +1,28 @@
+package com.kafkasprout.backend;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.kafkasprout.backend.controllers.ClusterController;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(ClusterController.class)
+//tag::test[]
+public class WebLayerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    //placeholder
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("")));
+    }
+}
