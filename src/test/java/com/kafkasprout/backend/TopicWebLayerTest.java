ec5118a8d22ebdb53227932bdd1817a21d5e3957
Guest <Guest@Mac.local>
2020-08-06 16:54:05 -0400

    more junit boiler

---
+++ src/test/java/com/kafkasprout/backend/TopicWebLayerTest.java
@@ -0,0 +1,23 @@
+package com.kafkasprout.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class TopicWebLayerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    // placeholder
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("")));
+    }
+}
